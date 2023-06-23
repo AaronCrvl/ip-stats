38c57c41d3cb9e8c1ac192595bf3f76034f3abd1
Aaron <72924198+AaronCrvl@users.noreply.github.com>
2023-06-23 12:24:06 -0300

    🔧updating configuration
    - tailwind themes update
    - nodemon dev tag order switch

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
-  },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
+  },  
   plugins: [],
 }
 
